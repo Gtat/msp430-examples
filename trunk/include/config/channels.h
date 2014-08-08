4b834a9c5618871a16b0f439e214a69cb1310b3f
Sam Boling <charles.samuel.boling@gmail.com>
2014-08-08 18:07:58 -0400

    Refactoring for extensibility, thread structure

---
+++ trunk/include/config/channels.h
@@ -0,0 +1,10 @@
+#ifndef __CHANNELS_H_GUARD
+#define __CHANNELS_H_GUARD
+
+#define CH0_GAS_1 1 /* O2       */
+#define CH3_GAS_4 1 /* H2 / SO2 */
+#define CH5_GAS_3 1 /* CH4      */
+#define CH6_TEMP  1 /* temperature */
+#define CH7_HUMID 1 /* humidity    */
+
+#endif /* __CHANNELS_H_GUARD */
