080e3a19a43ce513cddebdac65a04f1970640541
Sam Boling <charles.samuel.boling@gmail.com>
2015-01-26 17:21:27 -0500

    fixed typo in memory destination identifiers

--- trunk/src/interrupt.c
+++ trunk/src/interrupt.c
@@ -2,7 +2,7 @@
  * @file   interrupt.c
  * @author Sam Boling <charles.samuel.boling@gmail.com>
  * @version 0.1
- * 
+ *
  * @section DESCRIPTION
  *
  * Interrupt routines for the iEGAS software platform. These govern (most of)
@@ -13,11 +13,10 @@
  * Issued when the USCI is ready to write another byte. Has the same operation
  * in both SPI and RS232 modes.
  */
-#pragma vector=USCIAB0TX_VECTOR
-__interrupt void usci_tx_isr
+void __attribute__(( interrupt(USCIAB0TX_VECTOR) )) usci_tx_isr
   (void)
 {
-  UCA0TXBUF = RING_QUEUE_POP(outgoing_comm_q); 
+  UCA0TXBUF = RING_QUEUE_POP(outgoing_comm_q);
   if (RING_QUEUE_EMPTY(outgoing_comm_q))
   {
     UC0IE &= ~UCA0TXIE;
@@ -27,11 +26,10 @@ __interrupt void usci_tx_isr
 /**
  * Issued when the USCI has receieved a byte. In SPI mode, this indicates that
  * a byte has finished sending. In RS232 mode, this indicates that a byte was
- * received from the PC. In either case we must wake up the processor if a 
+ * received from the PC. In either case we must wake up the processor if a
  * packet has been completed.
  */
-#pragma vector=USCIAB0RX_VECTOR
-__interrupt void usci_rx_isr
+void __attribute__(( interrupt(USCIAB0RX_VECTOR) )) usci_rx_isr
   (void)
 {
   if (UCA0CTL0 & UCSYNC) /* SPI mode */
@@ -44,7 +42,7 @@ __interrupt void usci_rx_isr
   }
   else
   {
-    RING_QUEUE_PUSH(incoming_comm_q, UCA0RXBUF); 
+    RING_QUEUE_PUSH(incoming_comm_q, UCA0RXBUF);
     if (incoming_comm_q.length >= sizeof(union pc_to_mcu))
     {
       control.pc_packets++;
@@ -56,11 +54,10 @@ __interrupt void usci_rx_isr
 /**
  * Issued when an ADC conversion is complete. The ADC peripheral is
  * configured to sample all channels and automatically transfer results
- * into memory, so this indicates that a sample has been taken from 
+ * into memory, so this indicates that a sample has been taken from
  * each channel and is ready for processing, storage, or transferral.
  */
-#pragma vector=ADC10_VECTOR
-__interrupt void adc_isr
+void __attribute__(( interrupt(ADC10_VECTOR) )) adc_isr
   (void)
 {
   RING_QUEUE_PUSH_NO_DATA(sample_q);
@@ -68,8 +65,7 @@ __interrupt void adc_isr
   __bic_SR_register_on_exit(LPM0_bits);
 }
 
-#pragma vector=TIMER1_A0_VECTOR
-__interrupt void timer1_a0_isr
+void __attribute__(( interrupt(TIMER1_A0_VECTOR) )) timer1_a0_isr
   (void)
 {
 #ifdef CONFIG_ENABLE_DYNAMIC_BIASING
@@ -96,4 +92,3 @@ __interrupt void timer1_a0_isr
   control.seconds--;
 #endif /* #ifdef CONFIG_ENABLE_DYNAMIC_BIASING */
 }
-
