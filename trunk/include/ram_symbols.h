4cb7cf98880853932a0f3c6fcde66412142b67d9
Sam Boling <charles.samuel.boling@gmail.com>
2015-02-13 09:27:59 -0500

    understanding yuning's script, added some comments

--- trunk/include/ram_symbols.h
+++ trunk/include/ram_symbols.h
@@ -18,8 +18,15 @@ extern uint8_t ram_code_rom_end;
 extern uint8_t ram_code[RAM_CODE_LENGTH]
   __attribute__ (( section(".ram_symbols_in_ram") ));
 
+/* compute the offset of a given symbol
+ * from the start of the reserved RAM region
+ * given its static (ROM) address
+ */
 #define OFFSET_OF_RAM_SYMBOL(ptr) \
   ((size_t)(ptr) - (size_t)&ram_code_rom_begin)
+/* produces a reference to an identifier that lives in RAM
+ * given its static (ROM) address
+ */
 #define RAM_CODE_PTR(s)                                                \
   ({                                                                   \
      typeof(&s) __p __attribute__(( unused ));                         \
@@ -30,4 +37,3 @@ void ram_routine_load
   (void);
 
 #endif /* __RAM_ROUTINES_H_GUARD */
-
